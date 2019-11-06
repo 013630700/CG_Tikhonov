47740455287db203dd20a4db79ca84fc559280d2
013630700 <salla.latva-aijo@helsinki.fi>
2019-11-06 11:00:15 +0200

    Update A2x2Tmult.m

--- A2x2Tmult.m
+++ A2x2Tmult.m
@@ -1,6 +1,6 @@
-function [ res ] = A2x2Tmult( A,c11,c12,c21,c22,m1,m2 )
+function [ res ] = A2x2Tmult( a,c11,c12,c21,c22,m1,m2 )
 % Make combination matrix A^T and multiply with vector m
-AT = [c11*A.' c12*A.'; c21*A.' c22*A.'];
-m = [m1(:);m2(:)];
+AT = [c11*a.' c21*a.'; c12*a.' c22*a.'];
+m = [m1; m2];
 res = AT*m;
 end
